b1d5bbba00fab7b9794341941f6cbd1d878461d5
Leo Tumwattana <leo.tumwattana@gmail.com>
2022-06-15 02:05:29 +0800

    Add sample code

--- iOS16-BottomSheet-SwiftUI/ContentView.swift
+++ iOS16-BottomSheet-SwiftUI/ContentView.swift
@@ -7,13 +7,43 @@
 
 import SwiftUI
 
+extension PresentationDetent {
+    static var closed: PresentationDetent = .height(60)
+    static var opened: PresentationDetent = .height(400)
+}
+
 struct ContentView: View {
+    
+    @State private var isPresented: Bool = true
+    @State private var selectedDetent: PresentationDetent = .closed
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+                .sheet(isPresented: $isPresented) {
+                    VStack {
+                        
+                        // Tapping this button changes selectedDetent.
+                        // Expect that the detent to update to binded value,
+                        // however, currently this does nothing.
+                        Button("Toggle Sheet Detent") {
+                            if selectedDetent == .opened {
+                                selectedDetent = .closed
+                                let _ = print("Should close: selectedDetent value: \(selectedDetent)")
+                            } else {
+                                selectedDetent = .opened
+                                let _ = print("Should open: selectedDetent value: \(selectedDetent)")
+                            }
+                        }
+                        Spacer()
+                    }
+                    .padding(.top, 32)
+                    .presentationDetents(
+                        [.closed, .opened],
+                        selection: $selectedDetent
+                    )
+                }
         }
     }
 }
