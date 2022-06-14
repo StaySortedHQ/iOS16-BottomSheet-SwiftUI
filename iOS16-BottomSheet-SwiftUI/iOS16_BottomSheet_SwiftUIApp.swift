ff3bbf6aa25dfb6587df0ca836e2cc7688a200bb
Leo Tumwattana <leo.tumwattana@gmail.com>
2022-06-15 01:47:20 +0800

    Initial Commit

---
+++ iOS16-BottomSheet-SwiftUI/iOS16_BottomSheet_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16_BottomSheet_SwiftUIApp.swift
+//  iOS16-BottomSheet-SwiftUI
+//
+//  Created by Leo Tumwattana on 15/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS16_BottomSheet_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
